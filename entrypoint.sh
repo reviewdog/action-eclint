b46bb56a1a5e865727178b168a84a314f392bdc0
Zadkiel <hello@zadkiel.fr>
2020-01-02 18:00:37 +0100

    fix: send logs to temp folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,10 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 eclint --version
 
+TARGET_FOLDER=$(mktemp -d -t reviewdog-eclint-XXXXXXXXXX)
+
 # Make eclint think that we are Jenkins to get Checksuite result
-env JENKINS_URL=1 BUILD_ID=1 CI_REPORTS=/tmp/reports \
+JENKINS_URL=1 BUILD_ID=1 CI_REPORTS=$TARGET_FOLDER \
   eclint check ${INPUT_ECLINT_FLAGS:-'.'} 2>/dev/null \
-  || cat lint-reports/*/checkstyle-result.xml \
+  || cat "$TARGET_FOLDER/*/checkstyle-result.xml" \
   | reviewdog -f=checkstyle -name "eclint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
