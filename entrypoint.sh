aef2088aef20219ddde7cf693741a828429af0f5
Zadkiel <hello@zadkiel.fr>
2020-01-02 18:13:30 +0100

    fix: use fallback to success when there are no results

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,11 @@ eclint --version
 
 TARGET_FOLDER=$(mktemp -d -t reviewdog-eclint-XXXXXXXXXX)
 
-# Make eclint think that we are Jenkins to get Checksuite result
-JENKINS_URL=1 BUILD_ID=1 CI_REPORTS=$TARGET_FOLDER \
-  eclint check ${INPUT_ECLINT_FLAGS:-'.'} 2>/dev/null \
-  || cat "$TARGET_FOLDER/*/checkstyle-result.xml" \
-  | reviewdog -f=checkstyle -name "eclint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+# Make eclint think that we are Jenkins to get Checksuite type output
+# Extract result from file or fallback if file does not exist
+
+JENKINS_URL=1 BUILD_ID=1 CI_REPORTS="$TARGET_FOLDER" \
+  eclint check ${INPUT_ECLINT_FLAGS:-'.'} 2>/dev/null || \
+    cat "$TARGET_FOLDER/*/checkstyle-result.xml" 2>/dev/null || \
+    echo '<checkstyle></checkstyle>' | \
+      reviewdog -f=checkstyle -name="eclint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
